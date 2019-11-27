9e51c892e4a294df54674a7865fbe38f090a0195
Pulak Kumar Das <pulak1901@gmail.com>
2019-11-27 13:25:09 +0530

    Added 2nd model for testing.

--- models.py
+++ models.py
@@ -2,12 +2,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Activation
+from keras.layers import TimeDistributed
+from keras.layers import Dropout
 
 def nn_01(network_input, num_pitches):
     """ Super basic neural network for text generation taken from
         https://keras.io/examples/lstm_text_generation/ """
     model = Sequential()
     model.add(LSTM(128, input_shape=(network_input.shape[1], network_input.shape[2])))
+    model.add(Dense(num_pitches))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    return model
+
+def nn_02(network_input, num_pitches):
+    """ Double the number of LSTM cells as nn_01 """
+    model = Sequential()
+    model.add(LSTM(256, input_shape=(network_input.shape[1], network_input.shape[2])))
     model.add(Dense(num_pitches))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
--- train_and_generate.py
+++ train_and_generate.py
@@ -4,42 +4,41 @@ import models
 import time
 
 # Variables
-get_nn = models.nn_01
-fname = "01-01"
+get_nn = models.nn_02
+fname = "02-01"
 sequence_length = 100
 generated_length = 500
 num_epochs = 100
-batch_size = 32
+batch_size = 64
 
 # Constants
 path_to_notes = "./generated/data/notes"
 path_to_weights = "./generated/data/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
 path_to_data = "./dataset/*.mid"
 path_to_midi = "./generated/%s.mid" % fname
-path_to_log = "./generated/%s.log" % fname
 
-if __name__ == '__main__':
-    before_training = time.time()
+before_training = time.time()
 
-    trainer = Trainer(path_to_data, path_to_notes, path_to_weights, sequence_length)
-    nn_input, nn_output = trainer.prepare()
-    normalized_input = trainer.get_normalized_input(nn_input)
+trainer = Trainer(path_to_data, path_to_notes, path_to_weights, sequence_length)
+nn_input, nn_output = trainer.prepare()
+normalized_input = trainer.get_normalized_input(nn_input)
 
-    model = get_nn(normalized_input, trainer.num_pitches)
-    trainer.train_model(model, normalized_input, nn_output, batch_size, num_epochs)
+model = get_nn(normalized_input, trainer.num_pitches)
+trainer.train_model(model, normalized_input, nn_output, batch_size, num_epochs)
 
-    after_training = time.time()
+after_training = time.time()
 
-    # Load the weights to each node (if the prediction is being done later)
-    # model.load_weights(path_to_weights)
-    prediction = Prediction(trainer.notes.array, trainer.pitch_names, trainer.num_pitches)
-    prediction_output = prediction.generate_notes(
-        model, nn_input, generated_length
-    )
-    prediction.generate_midi(prediction_output, path_to_midi)
+# Load the weights to each node (if the prediction is being done later)
+# model.load_weights(path_to_weights)
 
-    after_prediction = time.time()
+prediction = Prediction(trainer.notes.array, trainer.pitch_names, trainer.num_pitches)
 
-    with open(path_to_log, "w") as f:
-        f.write("Training: ", after_training - before_training)
-        f.write("Prediction: ", after_prediction - after_training)
+prediction_output = prediction.generate_notes(
+    model, nn_input, generated_length
+)
+
+prediction.generate_midi(prediction_output, path_to_midi)
+after_prediction = time.time()
+
+print("Training Time: ", after_training - before_training)
+print("Prediction Time: ", after_prediction - after_training)
